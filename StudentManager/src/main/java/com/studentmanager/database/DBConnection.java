f4804e4153764b45f3f0c97ff27d2846a266f07e
WacoderForever <sethomondi088@gmail.com>
2025-02-27 21:04:57 +0300

    succesful database connection

---
+++ StudentManager/src/main/java/com/studentmanager/database/DBConnection.java
@@ -0,0 +1,24 @@
+package com.studentmanager.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    static {
+        try {
+            // Explicitly register the MariaDB driver
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load MariaDB driver", e);
+        }
+    }
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/student_manager";
+    private static final String USER = "root";
+    private static final String PASSWORD = "seth2019"; 
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ StudentManager/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Manager</title>
+</head>
+<body>
+    <h1>Welcome to Student Manager</h1>
+    <a href="test">Test Database Connection</a>
+</body>
+</html>
